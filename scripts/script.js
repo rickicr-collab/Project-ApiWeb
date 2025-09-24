0c86e87dd796bb2fb903952070aef2588b006895
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-09-24 10:26:01 -0300

    finalized front-end of the project

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const formulario = document.querySelector('form');
+
+const Inome = document.querySelector('.nome');
+const Iemail = document.querySelector('.email');
+const Isenha = document.querySelector('.senha');
+const Itelefone = document.querySelector('.telefone');
+
+
+
+function cadastrar() {
+    fetch("http://localhost:8080/cadastrar",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                nome: Inome.value,
+                email: Iemail.value,
+                senha: Isenha.value,
+                telefone: Itelefone.value
+            })
+            
+        })
+        .then(function (res) { console.log(res) })
+        .catch(function (res) { console.log(res) })
+};
+
+
+function limparCampos(){
+    Inome.value = "";
+    Iemail.value = "";
+    Isenha.value = "";
+    Itelefone.value = "";
+}
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cadastrar();
+    limparCampos();
+});
