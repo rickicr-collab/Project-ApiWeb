2f127d6162b2454d8a233d2e3463e66dd01ae9d8
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-09-29 09:57:35 -0300

    last modifields of endpoints for backend and modifield file script.sql

--- Projeto/src/main/java/br/com/criandoApi/Projeto/controller/UsuarioController.java
+++ Projeto/src/main/java/br/com/criandoApi/Projeto/controller/UsuarioController.java
@@ -1,26 +1,48 @@
 package br.com.criandoApi.Projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoApi.Projeto.dao.IUsuario;
 import br.com.criandoApi.Projeto.model.Usuario;
 
 @RestController
-public class UsuarioController  {
+@RequestMapping("/usuarios")
+public class UsuarioController {
     @Autowired
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
-    public List<Usuario> listaUsuarios(){
+    @GetMapping
+    public List<Usuario> listaUsuarios() {
         return (List<Usuario>) dao.findAll();
     }
 
-    
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = dao.save(usuario);
+        return novoUsuario;
+    }
 
+    @PutMapping
+    public Usuario atualizarUsuario(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = dao.save(usuario);
+        return novoUsuario;
+    }
 
-
+    @DeleteMapping({"/{id}"})
+    public Optional<Usuario> excluirUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+    }
 }
